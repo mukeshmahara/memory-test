410d7982a24f860705dbf2bb9983780b44e31b60
mukeshmahara <mukeshmahara1@gmail.com>
2023-02-26 12:36:42 +0545

    Display all the card initially for 2 sec

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,19 +1,52 @@
 import React from "react";
 
-function Card({ number, handleChoice, flipped }) {
-  const handleClick = () => {
+function Card({ number, handleChoice, flipped, visible }) {
+  const handleClick = (e) => {
     handleChoice(number);
   };
 
   return (
     <div className="card">
       <div className={flipped ? "flipped" : " "}>
-        <div className="front text-7xl rounded h-full w-full content-center text-red-500  items-center ">
-          {number.num}
-        </div>
-        <div className=" back rounded " onClick={handleClick}>
-          <img src="favicon.ico" className=" h-full" />
-        </div>
+        {!visible ? (
+          <div className="
+            flex
+            justify-center
+            items-center
+            front text-7xl
+            rounded 
+            h-full
+            w-full 
+            text-gray-50 
+           ">
+            {number.num}
+          </div>
+        ) : (
+          <div
+            className=" 
+              flex
+              text-7xl 
+              bg-gray-900 
+              text-gray-50
+              justify-center
+              items-center
+              absolute 
+              rounded
+              h-full
+              w-full
+            "
+          >
+            {number.num}
+          </div>
+        )}
+
+        {!visible ? (
+          <div className={" back "} onClick={handleClick}>
+            <img src={"favicon.ico"} className={" h-full"} />
+          </div>
+        ) : (
+          <div className="back"> </div>
+        )}
       </div>
     </div>
   );
